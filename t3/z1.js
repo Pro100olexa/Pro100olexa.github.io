8d8d18fb628b78ec23275465cbfeef5c1a272342
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2022-06-12 13:44:55 +0300

    Add files via upload

---
+++ t3/z1.js
@@ -0,0 +1,30 @@
+﻿/*alert(`хелоу`);
+let age = confirm(`Вам є 33 роки?`);
+
+let a1 = `Вася`;
+let a2 = 10;
+
+let name = prompt(`Як називати презедента України?`);
+if(name == `Володимир`){
+	alert(`Правда`);
+}else{
+	alert(`неправда`);
+}
+let age = prompt(`Ціна картоплі?`)
+
+if(age > 14){
+	alert(`дорого!`);
+	}else{
+alert(`дешево!`);
+	}
+	
+alert(`Вітаю на сайті StydiLink`);
+	let name = prompt(`Ваше ім'я`);
+alert(`Привіт ${name}`);
+
+let a1 = confirm(`Скільки вам років`);
+if(a1 == true){
+	alert(`білети за 130грн`);
+	}else{
+alert(`білети за 70грн!`);
+	}*/
