416c6333506a4fd8597461506f92d7d528424463
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2022-06-19 13:44:05 +0300

    Add files via upload

---
+++ t4/z1.js
@@ -0,0 +1,43 @@
+ /*let name = prompt(`Як вас звати?`);
+ 
+ if(name == `Петро` || name == `петро`){
+	 alert(`Ти молодець`);
+ }else if(name == `Олексій`){
+	 alert(`Апчхи`);
+ }else{
+	 alert(`Привіт`);          
+ }
+ 
+ let name = prompt(`Ваше число?`);
+if (name > 0){
+	 alert(`Ведене число є додатнім`);
+ }else if (name < 0){
+	 alert(`Ведене число не є додатнім`);
+ }else if (name == 0){
+	 alert(`Ведене число число не є відємним та не є додатнім`);
+ }
+ let name = prompt (`колір світлофора `);
+ if (name == `green` || name == `зелений`){
+	 alert(`можна йти`);
+ }else if(name == `жовтий` || name == `yellow`){
+	 alert(`приготуйтесь`);
+ }else if (name  == `червоний` || name == `red`
+ ){
+	 alert(`stop`);
+ }*/
+ let name == prompt('4+5?');
+ if (name == `9`){
+	 
+ }
+ let name == prompt('9-4?');
+ if (name == `5`){
+	 
+ }
+ let name == confirm('9-5?');
+ if (name == `4`){
+	 
+ }
+ let name == confirm('5+4?');
+ if (name == `9`){
+	 
+ }
