529910e7d6c392870cfceebbc18daa8ce52cef70
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2022-09-18 13:47:13 +0300

    Add files via upload

--- js6/z3.js
+++ js6/z3.js
@@ -1,5 +1,10 @@
 let x = [];
-for(i=1;i<6;i++){
-x[i] = prompt(`яка у вас оцінкка?`);
+name1 = `Petro`
+name2 = `матиматика`
+let suma = 0;
+
+for(i=0;i<5;i++){
+x[i] = Number(prompt(`яка у вас оцінкка?`));
+suma = suma + x[i];
 }
-console.log(x);
+console.log(`Вітаю ${name1}! У вас 5 оцінок з придмету ${name2}.Остання оцінка - ${x[x.length-1]}`);
---
+++ js6/z4.js
@@ -0,0 +1,14 @@
+
+/*let x = Number(prompt(`число?`));
+
+for(i=1; i<=x;i++) {
+console.log(`${i} ** ${x} = ${i**x}`)
+}*/
+
+let x = Number(prompt(`число?`));
+let suma = 0;
+
+for(i=1; i<=x; i++){
+suma = suma + i
+}
+console.log(suma);
